844da8cfecf143654234a58ca38d576dc62bb3c4
wenche <wenche@users.noreply.github.com>
2022-04-22 21:35:46 +0000

    deploy: 49bad34077864e193ecc3fa2c279a463a131d6aa

---
+++ _next/static/ORI_d18M0m-2o0_omkxTf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/144-9e8884a7ba04a2b7.js","static/css/3740438ea3d553db.css","static/chunks/pages/index-77b2b19954c45304.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
