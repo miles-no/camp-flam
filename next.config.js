ef56f472d07ba595d35729d44dc898f9e7503f51
Alexander Castillo <p.alexcast@gmail.com>
2022-04-22 14:33:21 +0200

    Separate local and prod build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const ghPagesSettings = process.env.GH_PAGES === 'true' ? {
+    basePath: '/camp-flam',
+    assetPrefix: '/camp-flam/',
+} : {};
+
 module.exports = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    ...ghPagesSettings
 }
