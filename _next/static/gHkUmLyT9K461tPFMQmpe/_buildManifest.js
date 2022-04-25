a9ef2a0284f3d43d3e2a240583df23122dadbdb0
palexcast <palexcast@users.noreply.github.com>
2022-04-25 05:59:08 +0000

    deploy: ab2582fbddddeea8a9972c85d937c12cb4872fa3

--- _next/static/gHkUmLyT9K461tPFMQmpe/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/144-9e8884a7ba04a2b7.js","static/chunks/pages/index-bdb00c725e015f70.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
