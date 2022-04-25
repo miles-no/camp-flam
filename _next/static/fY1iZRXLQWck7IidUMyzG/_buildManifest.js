5e4b2fcc9ef98402857ccdc0b9a5cb84a132f529
wenche <wenche@users.noreply.github.com>
2022-04-25 20:23:09 +0000

    deploy: e36f1975e3415afed9896febfd522b4842f0c78f

--- _next/static/fY1iZRXLQWck7IidUMyzG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0-f0e08dabeb27e29c.js","static/chunks/pages/index-584966fc23c2a4d9.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/mSk2XUAMrfIhdILGAr3_1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0-f0e08dabeb27e29c.js","static/chunks/pages/index-6b56e217bafcae57.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
