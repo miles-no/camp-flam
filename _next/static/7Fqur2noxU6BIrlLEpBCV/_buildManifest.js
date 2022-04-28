325a826675f624413ade0281990fd610968d3770
frodener <frodener@users.noreply.github.com>
2022-04-28 12:14:46 +0000

    deploy: c9a32b8af090be41d8ad3031781da5354c12fcbc

---
+++ _next/static/7Fqur2noxU6BIrlLEpBCV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/174-9e8c82ceb94dd7d6.js","static/chunks/pages/index-036340947b0efd25.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
