835caa787394f4b0dda3915c25a18636e3dea221
wenche <wenche@users.noreply.github.com>
2022-04-30 13:40:21 +0000

    deploy: 0a6915953b0175391d8e1b720e2a7a03542decb8

---
+++ _next/static/c1VH-SRTAGN1DIMU8ICxx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/174-9e8c82ceb94dd7d6.js","static/chunks/pages/index-78f2188d14c27f32.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
